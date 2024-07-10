0aeefc9af94a71613fe3ca4b1628b8c8955cc19a
ALbertEO3 <marichalalberto292@gmail.com>
2024-07-09 21:05:47 -0400

    paginas

--- dataproduct.py
+++ dataproduct.py
@@ -1,75 +1,82 @@
 import streamlit as st
-from graficas_plotly import *
+from charts.graficas_plotly import *
 
-# estos están para más adelante
-# from graficas_plotly import exportaciones, fish
 
-st.title("¿Le gusta comer alimentos de mar?")
-opciones = st.selectbox(
-    "",
-    [
+def principal():
+    st.title("¿Le gusta comer alimentos de mar?")
+    opciones = st.selectbox(
         "",
-        "Pargo",
-        "Cherna",
-        "Túnidos",
-        "Bonito",
-        "Biajaiba",
-        "Machuelo",
-        "Rabirubia",
-        "Raya",
-        "Carpa",
-        "Tenca",
-        "Tilapia",
-        "Claria",
-        "Cobo",
-        "Ostión",
-        "Almeja",
-        "Langosta",
-        "Camaron de mar",
-        "Camaronicultura",
-        "Morallas",
-    ],
-)
+        [
+            "",
+            "Pargo",
+            "Cherna",
+            "Túnidos",
+            "Bonito",
+            "Biajaiba",
+            "Machuelo",
+            "Rabirubia",
+            "Raya",
+            "Carpa",
+            "Tenca",
+            "Tilapia",
+            "Claria",
+            "Cobo",
+            "Ostión",
+            "Almeja",
+            "Langosta",
+            "Camaron de mar",
+            "Camaronicultura",
+            "Morallas",
+        ],
+    )
 
-if opciones == "Pargo":
-    st.plotly_chart(pargo)
-if opciones == "Cherna":
-    st.plotly_chart(Cherna)
-if opciones == "Túnidos":
-    st.plotly_chart(tunidos)
-if opciones == "Bonito":
-    st.plotly_chart(bonitos)
-if opciones == "Biajaiba":
-    st.plotly_chart(biajaiba)
-if opciones == "Machuelo":
-    st.plotly_chart(machuelo)
-if opciones == "Rabirubia":
-    st.plotly_chart(rabirubia)
-if opciones == "Raya":
-    st.plotly_chart(raya)
-if opciones == "Carpa":
-    st.plotly_chart(carpa)
-if opciones == "Tenca":
-    st.plotly_chart(tenca)
-if opciones == "Tilapia":
-    st.plotly_chart(tilapia)
-if opciones == "Claria":
-    st.plotly_chart(claria)
-if opciones == "Cobo":
-    st.plotly_chart(cobo)
-if opciones == "Ostión":
-    st.plotly_chart(ostion)
-if opciones == "Almeja":
-    st.plotly_chart(almeja)
-if opciones == "Langosta":
-    st.plotly_chart(langosta)
-if opciones == "Camaron de mar":
-    st.plotly_chart(camaron_de_mar)
-if opciones == "Camaronicultura":
-    st.plotly_chart(camaronicultura)
-if opciones == "Morallas":
-    st.plotly_chart(moralla)
+    if opciones == "Pargo":
+        st.plotly_chart(pargo)
+    if opciones == "Cherna":
+        st.plotly_chart(Cherna)
+    if opciones == "Túnidos":
+        st.plotly_chart(tunidos)
+    if opciones == "Bonito":
+        st.plotly_chart(bonitos)
+    if opciones == "Biajaiba":
+        st.plotly_chart(biajaiba)
+    if opciones == "Machuelo":
+        st.plotly_chart(machuelo)
+    if opciones == "Rabirubia":
+        st.plotly_chart(rabirubia)
+    if opciones == "Raya":
+        st.plotly_chart(raya)
+    if opciones == "Carpa":
+        st.plotly_chart(carpa)
+    if opciones == "Tenca":
+        st.plotly_chart(tenca)
+    if opciones == "Tilapia":
+        st.plotly_chart(tilapia)
+    if opciones == "Claria":
+        st.plotly_chart(claria)
+    if opciones == "Cobo":
+        st.plotly_chart(cobo)
+    if opciones == "Ostión":
+        st.plotly_chart(ostion)
+    if opciones == "Almeja":
+        st.plotly_chart(almeja)
+    if opciones == "Langosta":
+        st.plotly_chart(langosta)
+    if opciones == "Camaron de mar":
+        st.plotly_chart(camaron_de_mar)
+    if opciones == "Camaronicultura":
+        st.plotly_chart(camaronicultura)
+    if opciones == "Morallas":
+        st.plotly_chart(moralla)
+
+def pagina2():
+    pass
 
 
-# st.plotly_chart(exportaciones)
-# st.plotly_chart(fish)
+pages = {
+    "Inicio": principal,
+    "Económico": pagina2,
+}
+
+selection = st.sidebar.radio("Ir a", list(pages.keys()))
+pages[selection]()
